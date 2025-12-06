df81b1900ea94e30d3d5eede73e2e9d4cfea2666
Mikołaj Młodzikowski <14093153+mkjmdski@users.noreply.github.com>
2025-12-06 10:09:39 +0100

    feat: fix output

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,21 @@
 locals {
   subdirectory = "wheelie"
-}
-
-
-resource "databricks_notebook" "this" {
-  for_each = {
+  notebooks = {
     pull_wheelie = {
       notebook_filename = "pull_wheelie.py"
       language          = "PYTHON"
     }
   }
+}
+
+
+resource "databricks_notebook" "this" {
+  for_each = local.notebooks
   path     = "${data.databricks_current_user.me.home}/${local.subdirectory}/${each.key}"
   language = each.value.language
   source   = "../notebooks/${each.value.notebook_filename}"
 }
 
-output "notebook_url" {
-  value = databricks_notebook.this.url
+output "notebook_urls" {
+  value = { for k, v in databricks_notebook.this : k => databricks_notebook.this[k].url }
 }
